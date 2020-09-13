faec8348d4f3f20fc5c98db6e5fa4ecc97f86bfd
Sri3589 <train2selenium@gmail.com>
2020-09-13 13:41:21 -0400

    FirstFlyThereYouGo

--- TryTestingGit/src/TestGit/Browser.java
+++ TryTestingGit/src/TestGit/Browser.java
@@ -5,6 +5,7 @@ public class Browser {
 	public static void main(String[] args) {
 		System.out.print("Hi Git!!");
 		System.out.println("I Updated this file & You are Welcome!!!");
+		System.out.println("Seriously did you update. Ok Thank You!!!");
 	}
 
 }
